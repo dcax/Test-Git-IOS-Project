30b08a4802476cd0ae95312cdbe3aa86a1e73eb6
The Alexanders <Alexanders@Thes-iMac.local>
2013-11-03 10:40:30 -0600

    added a test class

---
+++ GitTest/DCATest.h
@@ -0,0 +1,13 @@
+//
+//  DCATest.h
+//  GitTest
+//
+//  Created by The Alexanders on 11/3/13.
+//  Copyright (c) 2013 The Alexanders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCATest : NSObject
+
+@end
